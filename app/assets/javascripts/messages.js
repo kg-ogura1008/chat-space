e7766aba333896673dae1ebad4e60c00f3bb00fb
Keiji Ogura <kg.ogura1008@gmail.com>
2018-03-30 12:09:36 +0900

    change_message.js

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -12,26 +12,39 @@ $(function(){
                   <p>
                     ${message.text}
                   </p>
-                  <image src="${message.image_url}">
                 </div>`
     return html;
   }
 
-  $('new_message').on('submit', function(e){
+  $('#new_message').on('submit', function(e){
     e.preventDefault();
-    var formData = new FromData(this);
+    var formData = new FormData(this);
     var url = $(this).attr('action')
+    console.log(this);
     $.ajax({
-      url: href,
+      url: url,
       type: "POST",
       data: formData,
       dataType: 'json',
       processData: false,
-      contentType: false
+      contentType: false.
+      beforeSend: function(xhr, settings){
+        $button.attr('disabled', true);
+      },
+      complete: function(xhr, textStatus){
+        $button.attr('disabled', false);
+      }
     })
     .done(function(data){
+      console.log(data.name);
+      console.log(data.date);
+      console.log(data.text);
       var html = buildHTML(data);
       $('.main_content__body').append(html)
+      $('.post__text').val('')
+    })
+    .fail(function(){
+      alert('error');
     })
   })
 });
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,17 +8,15 @@ class MessagesController < ApplicationController
 
   def create
     @message = @group.messages.new(message_params)
-
     if @message.save
-      redirect_to group_messages_path(@group), notice: 'メッセージが通知されました'
+      respond_to do |format|
+        format.html { redirect_to group_messages_path(params[:group_id]) }
+        format.json
+      end
     else
       @message = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください'
       render :index
-    end
-
-    respond_to do |format|
-      format.json
     end
   end
 
